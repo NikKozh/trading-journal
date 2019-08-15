53b58f80bcf24ed212518d037e1142b59107f305
zitt <NKtargeting@yandex.ru>
2019-08-16 00:25:05 +0500

    Create contract form view

--- app/controllers/ContractController.scala
+++ app/controllers/ContractController.scala
@@ -1,11 +1,16 @@
 package controllers
 
+import helpers.ContractHelper
 import javax.inject._
 import play.api.mvc._
 
 @Singleton
-class ContractController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
-    def index: Action[AnyContent] = Action {
+class ContractController @Inject()(mcc: MessagesControllerComponents) extends MessagesAbstractController(mcc) {
+    def contractList: Action[AnyContent] = Action {
         Ok(views.html.index("Hello, World!"))
+    }
+
+    def createContract: Action[AnyContent] = Action { implicit request: MessagesRequest[AnyContent] =>
+        Ok(views.html.createContract(ContractHelper.ContractForm.form))
     }
 }
