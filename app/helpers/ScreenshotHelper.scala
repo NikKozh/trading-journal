9ed13e9a3d6755109d724de9a4c1eb685c99f6ab
zitt <NKtargeting@yandex.ru>
2019-08-31 21:19:02 +0500

    Add screenshot cropping

--- app/helpers/ScreenshotHelper.scala
+++ app/helpers/ScreenshotHelper.scala
@@ -2,20 +2,25 @@ package helpers
 
 import java.util.Base64
 import java.net.URL
-
-import scala.sys.process._
+import javax.imageio.ImageIO
 
 object ScreenshotHelper {
-    def screenshotFromUrlToBase64(url: String): Option[String] = {
+    def screenshotFromUrlToBase64(urlString: String): Option[(String, String)] = {
         // TODO: обернуть в Try
-        val outputStream = new java.io.ByteArrayOutputStream()
-        val processResult = new URL(url) #> outputStream !
+        val url = new URL(urlString)
+        val fullImageOS = new java.io.ByteArrayOutputStream()
+        val croppedImageOS = new java.io.ByteArrayOutputStream()
+        val fullImage = ImageIO.read(url)
+        val croppedImage = fullImage.getSubimage(0, 0, 500, 40)
 
-        if (processResult == 0) {
-            val bytes = outputStream.toByteArray
-            val encodedScreenshot = Base64.getEncoder.encodeToString(bytes)
-            Some(encodedScreenshot)
-        } else
-            None
+        ImageIO.write(fullImage, "png", fullImageOS)
+        ImageIO.write(croppedImage, "png", croppedImageOS)
+
+        val fullImageBytes = fullImageOS.toByteArray
+        val croppedImageBytes = croppedImageOS.toByteArray
+        val encodedFullImage = Base64.getEncoder.encodeToString(fullImageBytes)
+        val encodedCroppedImage = Base64.getEncoder.encodeToString(croppedImageBytes)
+
+        Some((encodedFullImage, encodedCroppedImage))
     }
 }
