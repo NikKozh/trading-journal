c0312c3aa9fe565e28032b29ee5f01db0d66c961
zitt <NKtargeting@yandex.ru>
2020-01-24 02:36:42 +0500

    Add weekly stats + date formatter

--- app/controllers/StatsController.scala
+++ app/controllers/StatsController.scala
@@ -1,7 +1,10 @@
 package controllers
 
+import java.time.{DayOfWeek, LocalDate}
+import java.time.temporal.WeekFields
+
 import javax.inject.{Inject, Singleton}
-import models.stats.GeneralStatsDailyItem
+import models.stats.{GeneralStatsDailyItem, GeneralStatsWeeklyItem}
 import utils.Utils.DateTime._
 import play.api.Environment
 import play.api.mvc.{Action, AnyContent, MessagesAbstractController, MessagesControllerComponents}
@@ -20,6 +23,11 @@ class StatsController @Inject()(mcc: MessagesControllerComponents,
 
     def generalStats(): Action[AnyContent] = asyncActionWithExceptionPage {
         contractService.list.map { contractList =>
+            @scala.annotation.tailrec
+            def findFirstDayOfTheWeekForDate(date: LocalDate): LocalDate =
+                if (date.getDayOfWeek == DayOfWeek.MONDAY) date
+                else findFirstDayOfTheWeekForDate(date minusDays 1)
+
             // TODO: стопроцентов это можно сделать как-то получше - подумать над этим
             val dailyStatsItems =
                 contractList
@@ -29,16 +37,42 @@ class StatsController @Inject()(mcc: MessagesControllerComponents,
                     .map { case (day, contracts) =>
                         GeneralStatsDailyItem(
                             day,
-                            contracts.flatMap(_.income).sum,
-                            contracts.size,
-                            contracts.count(_.isWin)
+                            income = contracts.flatMap(_.income).sum,
+                            contractsCount = contracts.size,
+                            winningContracts = contracts.count(_.isWin)
                         )
                     }
                     .toSeq
                     .sortBy(_.day)
                     .reverse
 
-            Ok(views.html.stats.generalStats(dailyStatsItems))
+            val weeklyStatsItems =
+                dailyStatsItems
+                    .groupBy(_.day.getYear)
+                    .mapValues(_
+                        .groupBy(_.day.get(WeekFields.ISO.weekOfYear()))
+                        .values
+                        .toSeq
+                        .map { dailyStatsItemsOnWeek =>
+                            val sortedItems = dailyStatsItemsOnWeek.sortBy(_.day)
+                            val firstDayOfWeek = findFirstDayOfTheWeekForDate(sortedItems.head.day)
+                            val lastDayOfWeek = firstDayOfWeek plusDays 6
+
+                            GeneralStatsWeeklyItem(
+                                daysRange = (firstDayOfWeek, lastDayOfWeek),
+                                income = sortedItems.map(_.income).sum,
+                                contractsCount = sortedItems.map(_.contractsCount).sum,
+                                winningContracts = sortedItems.map(_.winningContracts).sum
+                            )
+                        }
+                    )
+                    .values
+                    .toSeq
+                    .flatten
+                    .sortBy(_.daysRange._1)
+                    .reverse
+
+            Ok(views.html.stats.generalStats(dailyStatsItems, weeklyStatsItems))
         }
     }
 }
