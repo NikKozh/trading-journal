658ce6852c9a3459e5e8e1f368eee2fbaad7c2c8
NikKozh <nkozhevnikov@naumen.ru>
2020-04-14 01:34:13 +0500

    Remove OCR

--- app/helpers/ScreenshotHelper.scala
+++ app/helpers/ScreenshotHelper.scala
@@ -5,22 +5,17 @@ import java.net.URL
 import javax.imageio.ImageIO
 
 object ScreenshotHelper {
-    def screenshotFromUrlToBase64(urlString: String): Option[(String, String)] = {
+    def screenshotFromUrlToBase64(urlString: String): Option[String] = {
         // TODO: обернуть в Try
         val url = new URL(urlString)
         val fullImageOS = new java.io.ByteArrayOutputStream()
-        val croppedImageOS = new java.io.ByteArrayOutputStream()
         val fullImage = ImageIO.read(url)
-        val croppedImage = fullImage.getSubimage(0, 0, 500, 40)
 
         ImageIO.write(fullImage, "png", fullImageOS)
-        ImageIO.write(croppedImage, "png", croppedImageOS)
 
         val fullImageBytes = fullImageOS.toByteArray
-        val croppedImageBytes = croppedImageOS.toByteArray
         val encodedFullImage = Base64.getEncoder.encodeToString(fullImageBytes)
-        val encodedCroppedImage = Base64.getEncoder.encodeToString(croppedImageBytes)
 
-        Some((encodedFullImage, encodedCroppedImage))
+        Some(encodedFullImage)
     }
 }
