2df41866028e6a36d00d5f5aa187abbb54002914
NikKozh <nkozhevnikov@naumen.ru>
2020-05-05 20:23:48 +0500

    Add COR filter settings and make test ping between frontend and backend

--- tj-frontend/src/tj-frontend-app/tj-frontend-app.js
+++ tj-frontend/src/tj-frontend-app/tj-frontend-app.js
@@ -5,24 +5,43 @@ import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
  * @polymer
  */
 class TjFrontendApp extends PolymerElement {
-  static get template() {
-    return html`
-      <style>
-        :host {
-          display: block;
-        }
-      </style>
-      <h2>Hello [[prop1]]!</h2>
-    `;
-  }
-  static get properties() {
-    return {
-      prop1: {
-        type: String,
-        value: 'tj-frontend-app'
-      }
-    };
-  }
+    static get template() {
+        // language=HTML
+        return html`
+            <style>
+                :host {
+                    display: block;
+                }
+            </style>
+            <h2>Message: [[prop1.message]]</h2>
+            <h2>Status: [[prop1.status]]</h2>
+        `;
+    }
+
+    static get properties() {
+        return {
+            prop1: {
+                type: Object,
+                value: { "message": "No message provided yet", "status": "N/A" }
+            }
+        };
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+
+        fetch("http://localhost:9000/ping")
+            .then(
+                r => r.json(),
+                err => {
+                    return {"message": `Problem on server side. Error: ${err}`, "status": "Down"}
+                }
+            )
+            .then(
+                json => this.prop1 = json,
+                err => this.prop1 = {"message": `Problem on parsing json. Error: ${err}`, "status": "Down"}
+            )
+    }
 }
 
 window.customElements.define('tj-frontend-app', TjFrontendApp);
